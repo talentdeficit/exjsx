8850e0655fbf68f7f847d777175c2d9dd40463a2
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-06-03 04:12:27 +0000

    moved stuff around

--- lib/jsx.ex
+++ lib/jsx.ex
@@ -1,7 +1,7 @@
 defmodule JSX do
   def encode(term, opts // []) do
     parser_opts = :jsx_config.extract_config(opts ++ [:escaped_strings])
-    :jsx.parser(:jsx_to_json, opts, parser_opts).(List.flatten(JSX.Encode.json(term) ++ [:end_json]))
+    :jsx.parser(:jsx_to_json, opts, parser_opts).(List.flatten(JSXEncoder.json(term) ++ [:end_json]))
   end
   
   def decode(json, opts // []) do
@@ -62,45 +62,45 @@ defmodule JSX do
       :erlang.binary_to_atom(key, :utf8)
     end
   end
+end
   
-  defprotocol Encode do
-    @only [List, Tuple, Atom, Number, BitString, Any]
-    def json(term)
+defprotocol JSXEncoder do
+  @only [List, Tuple, Atom, Number, BitString, Any]
+  def json(term)
+end
+
+defimpl JSXEncoder, for: List do
+  def json([]), do: [:start_array, :end_array]
+  def json([{}]), do: [:start_object, :end_object]
+  def json([first|_] = list) when is_tuple(first) do
+    [:start_object] ++ List.flatten(Enum.map(list, fn(term) -> JSXEncoder.json(term) end)) ++ [:end_object]
   end
-  
-  defimpl Encode, for: List do
-    def json([]), do: [:start_array, :end_array]
-    def json([{}]), do: [:start_object, :end_object]
-    def json([first|_] = list) when is_tuple(first) do
-      [:start_object] ++ List.flatten(Enum.map(list, fn(term) -> JSX.Encode.json(term) end)) ++ [:end_object]
-    end
-    def json(list) do
-      [:start_array] ++ List.flatten(Enum.map(list, fn(term) -> JSX.Encode.json(term) end)) ++ [:end_array]
-    end
+  def json(list) do
+    [:start_array] ++ List.flatten(Enum.map(list, fn(term) -> JSXEncoder.json(term) end)) ++ [:end_array]
   end
-  
-  defimpl Encode, for: Tuple do
-    def json({key, value}) when is_atom(key), do: [{:key, key}] ++ JSX.Encode.json(value)
-    def json(_), do: raise ArgumentError
-  end
-  
-  defimpl Encode, for: Atom do
-    def json(:true), do: [:true]
-    def json(:false), do: [:false]
-    def json(:nil), do: [:null]
-    def json(_), do: raise ArgumentError
-  end
-  
-  defimpl Encode, for: Number do
-    def json(number) when is_integer(number), do: [number]
-    def json(number) when is_float(number), do: [number]
-  end
-  
-  defimpl Encode, for: BitString do
-    def json(string), do: [string]
-  end
-  
-  defimpl Encode, for: Any do
-    def json(_), do: raise ArgumentError
-  end
+end
+
+defimpl JSXEncoder, for: Tuple do
+  def json({key, value}) when is_atom(key), do: [{:key, key}] ++ JSXEncoder.json(value)
+  def json(_), do: raise ArgumentError
+end
+
+defimpl JSXEncoder, for: Atom do
+  def json(:true), do: [:true]
+  def json(:false), do: [:false]
+  def json(:nil), do: [:null]
+  def json(_), do: raise ArgumentError
+end
+
+defimpl JSXEncoder, for: Number do
+  def json(number) when is_integer(number), do: [number]
+  def json(number) when is_float(number), do: [number]
+end
+
+defimpl JSXEncoder, for: BitString do
+  def json(string), do: [string]
+end
+
+defimpl JSXEncoder, for: Any do
+  def json(_), do: raise ArgumentError
 end
