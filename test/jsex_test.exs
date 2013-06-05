28347e4c04f59c6eb19e477ff08e393e38f12465
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-06-05 13:46:29 +0000

    refactor tests to provide more structure

--- test/jsex_test.exs
+++ test/jsex_test.exs
@@ -1,70 +1,38 @@
 Code.require_file "test_helper.exs", __DIR__
 
-defmodule JSEXTest do
+defmodule JSEX.Tests.Helpers do
+  def numbers(:ex), do: [-18446744073709551617, -1.0, -1, 0, 0.0, 1, 1.0, 18446744073709551617]
+  def numbers(:json), do: "[-18446744073709551617,-1.0,-1,0,0.0,1,1.0,18446744073709551617]"
+end
+
+defmodule JSEX.Tests.Decode do
   use ExUnit.Case
+  import JSEX.Tests.Helpers
 
   test "decode empty object" do
     assert(JSEX.decode!("{}") == [{}])
   end
-  
-  test "encode empty object" do
-    assert(JSEX.encode!([{}]) == "{}")
-  end
-  
+
   test "decode empty list" do
     assert(JSEX.decode!("[]") == [])
   end
 
-  test "encode empty list" do
-    assert(JSEX.encode!([]) == "[]")
-  end
-  
-  test "encode list of empty lists" do
-    assert(JSEX.encode!([[], [], []]) == "[[],[],[]]")
-  end
-  
-  test "encode list of empty objects" do
-    assert(JSEX.encode!([[{}], [{}], [{}]]) == "[{},{},{}]")
-  end
-  
   test "decode literals" do
     assert(JSEX.decode!("[true, false, null]") == [true, false, nil])
   end
-  
-  test "encode literals" do
-    assert(JSEX.encode!([true, false, nil]) == "[true,false,null]")
-  end
-  
+
   test "decode numbers" do
-    assert(
-      JSEX.decode!("[-18446744073709551617, -1.0, -1, 0, 0.0, 1, 1.0, 18446744073709551617]")
-        == [-18446744073709551617, -1.0, -1, 0, 0.0, 1, 1.0, 18446744073709551617]
-      )
+    assert(JSEX.decode!(numbers(:json)) == numbers(:ex))
   end
-  
-  test "encode numbers" do
-    assert(
-      JSEX.encode!([-18446744073709551617, -1.0, -1, 0, 0.0, 1, 1.0, 18446744073709551617])
-        == "[-18446744073709551617,-1.0,-1,0,0.0,1,1.0,18446744073709551617]"
-      )
-  end
-  
+
   test "decode strings" do
     assert(JSEX.decode!("[\"hallo\", \"world\"]") == ["hallo", "world"])
   end
 
-  test "encode strings" do
-    assert(JSEX.encode!(["hallo", "world"]) == "[\"hallo\",\"world\"]")
-  end
-  
   test "decode simple object" do
     assert(JSEX.decode!("{\"key\": true}") == [{"key", true}])
   end
-  
-  test "encode simple object" do
-    assert(JSEX.encode!([key: true]) == "{\"key\":true}")
-  end
-  
+
   test "decode compound object" do
     assert(JSEX.decode!(
       "{\"a\": [ true, false, null ], \"b\": \"hallo world\", \"c\": {
@@ -80,7 +48,44 @@ defmodule JSEXTest do
       ]
     )
   end
-  
+end
+
+defmodule JSEX.Tests.Encode do
+  use ExUnit.Case
+  import JSEX.Tests.Helpers
+
+  test "encode empty object" do
+    assert(JSEX.encode!([{}]) == "{}")
+  end
+
+  test "encode empty list" do
+    assert(JSEX.encode!([]) == "[]")
+  end
+
+  test "encode list of empty lists" do
+    assert(JSEX.encode!([[], [], []]) == "[[],[],[]]")
+  end
+
+  test "encode list of empty objects" do
+    assert(JSEX.encode!([[{}], [{}], [{}]]) == "[{},{},{}]")
+  end
+
+  test "encode literals" do
+    assert(JSEX.encode!([true, false, nil]) == "[true,false,null]")
+  end
+
+  test "encode numbers" do
+    assert(JSEX.encode!(numbers(:ex)) == numbers(:json))
+  end
+
+  test "encode strings" do
+    assert(JSEX.encode!(["hallo", "world"]) == "[\"hallo\",\"world\"]")
+  end
+
+  test "encode simple object" do
+    assert(JSEX.encode!([key: true]) == "{\"key\":true}")
+  end
+
   test "encode compound object" do
     assert(JSEX.encode!([
         a: [true,false,nil],
@@ -94,25 +99,24 @@ defmodule JSEXTest do
       "{\"a\":[true,false,null],\"b\":\"hallo world\",\"c\":{\"x\":[1,2,3],\"y\":{},\"z\":[[[]]]}}"
     )
   end
-  
+
   defrecord SimpleRecord, name: nil, rank: nil
-  
+
   test "encode a simple record" do
     assert(JSEX.encode!(SimpleRecord.new(name: "Walder Frey", rank: "Lord"))
       == "{\"name\":\"Walder Frey\",\"rank\":\"Lord\"}")
   end
-  
+
   defrecord BasicRecord, name: nil, rank: nil
-  
+
   defimpl JSEX.Encoder, for: BasicRecord do
     def json(record) do
       [:start_object, "name", record.rank <> " " <> record.name, :end_object]
     end
   end
-  
+
   test "encode a basic record with a protocol defined" do
     assert(JSEX.encode!(BasicRecord.new(name: "Walder Frey", rank: "Lord"))
       == "{\"name\":\"Lord Walder Frey\"}")
   end
-  
 end
