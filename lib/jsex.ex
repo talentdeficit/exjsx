5502aefa030d38861572144ae72a2492f67f86c7
Devin Torres <devin@devintorr.es>
2013-06-05 10:39:02 -0500

    Use :lists.flatten
    See: https://github.com/elixir-lang/elixir/issues/948

--- lib/jsex.ex
+++ lib/jsex.ex
@@ -1,7 +1,9 @@
+import :lists, only: [flatten: 1]
+
 defmodule JSEX do
   def encode!(term, opts // []) do
     parser_opts = :jsx_config.extract_config(opts ++ [:escaped_strings])
-    :jsx.parser(:jsx_to_json, opts, parser_opts).(List.flatten(JSEX.Encoder.json(term) ++ [:end_json]))
+    :jsx.parser(:jsx_to_json, opts, parser_opts).(flatten(JSEX.Encoder.json(term) ++ [:end_json]))
   end
   
   def encode(term, opts // []) do
@@ -61,7 +63,7 @@ defmodule JSEX do
   
   def is_term?(term, opts // []) do
     parser_opts = :jsx_config.extract_config(opts)
-    :jsx.parser(:jsx_verify, opts, parser_opts).(List.flatten(JSEX.Encoder.json(term) ++ [:end_json]))
+    :jsx.parser(:jsx_verify, opts, parser_opts).(flatten(JSEX.Encoder.json(term) ++ [:end_json]))
   rescue
     _ -> false
   end
@@ -94,10 +96,10 @@ defimpl JSEX.Encoder, for: List do
   def json([]), do: [:start_array, :end_array]
   def json([{}]), do: [:start_object, :end_object]
   def json([first|_] = list) when is_tuple(first) do
-    [:start_object] ++ List.flatten(Enum.map(list, fn(term) -> JSEX.Encoder.json(term) end)) ++ [:end_object]
+    [:start_object] ++ flatten(Enum.map(list, fn(term) -> JSEX.Encoder.json(term) end)) ++ [:end_object]
   end
   def json(list) do
-    [:start_array] ++ List.flatten(Enum.map(list, fn(term) -> JSEX.Encoder.json(term) end)) ++ [:end_array]
+    [:start_array] ++ flatten(Enum.map(list, fn(term) -> JSEX.Encoder.json(term) end)) ++ [:end_array]
   end
 end
 
