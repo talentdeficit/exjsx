98e697de5dcd1071dc6020d3c8c29ee4c00ceaef
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-06-05 01:11:50 +0000

    minor refactoring to move some internals around

--- lib/jsex.ex
+++ lib/jsex.ex
@@ -62,19 +62,19 @@ defmodule JSEX do
   rescue
     _ -> false
   end
+end
    
-  defmodule Decoder do
-    def init(_) do
-      :jsx_to_term.init([])
-    end
-  
-    def handle_event({:literal, :null}, {[last|terms], config}) do
-      {[[nil] ++ last] ++ terms, config}
-    end
-  
-    def handle_event(event, config) do
-      :jsx_to_term.handle_event(event, config)
-    end
+defmodule JSEX.Decoder do
+  def init(_) do
+    :jsx_to_term.init([])
+  end
+
+  def handle_event({:literal, :null}, {[last|terms], config}) do
+    {[[nil] ++ last] ++ terms, config}
+  end
+
+  def handle_event(event, config) do
+    :jsx_to_term.handle_event(event, config)
   end
 end
   
