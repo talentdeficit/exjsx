e870b17fe168f5cc87505c88e578858532bd0d66
alisdair sullivan <alisdairsullivan@yahoo.ca>
2013-07-24 02:02:48 +0000

    add `package.exs` that got waylaid

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "jsex", description: "json for elixir",
+                 version: "0.0.1", keywords: [json], 
+                 maintainers: [[name: "alisdair sullivan", 
+                                email: "alisdair sullivan@yahoo.ca"]],
+                 repositories: [[github: "talentdeficit/jsex"]])
+    
