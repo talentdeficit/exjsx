01073fd976397d4140032b62ae64f15d1442ea18
alisdair sullivan <alisdairsullivan@yahoo.ca>
2014-05-01 01:23:03 +0000

    make internals private where possible

--- lib/jsex.ex
+++ lib/jsex.ex
@@ -98,45 +98,45 @@ defmodule JSEX.Decoder do
   def handle_event({ :literal, :null }, state), do: insert(:nil, state)
   def handle_event({ _, event }, state), do: insert(event, state)
 
-  def format_key(key, { _, :binary }), do: key
-  def format_key(key, { _, :atom }), do: :erlang.binary_to_atom(key, :utf8)
-  def format_key(key, { _, :existing_atom }), do: :erlang.binary_to_existing_atom(key, :utf8)
-  def format_key(key, { _, :attempt_atom }) do
+  defp format_key(key, { _, :binary }), do: key
+  defp format_key(key, { _, :atom }), do: :erlang.binary_to_atom(key, :utf8)
+  defp format_key(key, { _, :existing_atom }), do: :erlang.binary_to_existing_atom(key, :utf8)
+  defp format_key(key, { _, :attempt_atom }) do
     :erlang.binary_to_existing_atom(key, :utf8)
   rescue
     ArgumentError -> key
   end
   
-  def start_object({ stack, config }), do: { [{ :object, %{} }] ++ stack, config }
+  defp start_object({ stack, config }), do: { [{ :object, %{} }] ++ stack, config }
   
-  def start_array({ stack, config }), do: { [{ :array, [] }] ++ stack, config }
+  defp start_array({ stack, config }), do: { [{ :array, [] }] ++ stack, config }
     
-  def finish({ [{ :object, emptyMap }], config }) when is_map(emptyMap) and map_size(emptyMap) < 1 do
+  defp finish({ [{ :object, emptyMap }], config }) when is_map(emptyMap) and map_size(emptyMap) < 1 do
     { %{}, config }
   end
-  def finish({ [{ :object, emptyMap }|rest], config }) when is_map(emptyMap) and map_size(emptyMap) < 1 do
+  defp finish({ [{ :object, emptyMap }|rest], config }) when is_map(emptyMap) and map_size(emptyMap) < 1 do
     insert(%{}, { rest, config })
   end
-  def finish({ [{ :object, pairs }], config }), do: { pairs, config }
-  def finish({ [{ :object, pairs}|rest], config }), do: insert(pairs, { rest, config })
-  def finish({ [{ :array, values }], config }), do: { Enum.reverse(values), config }
-  def finish({ [{ :array, values}|rest], config}), do: insert(Enum.reverse(values), { rest, config })
-  def finish(_), do: raise ArgumentError
+  defp finish({ [{ :object, pairs }], config }), do: { pairs, config }
+  defp finish({ [{ :object, pairs}|rest], config }), do: insert(pairs, { rest, config })
+  defp finish({ [{ :array, values }], config }), do: { Enum.reverse(values), config }
+  defp finish({ [{ :array, values}|rest], config}), do: insert(Enum.reverse(values), { rest, config })
+  defp finish(_), do: raise ArgumentError
 
-  def insert(value, { [], config }), do: { value, config }
-  def insert(key, { [{ :object, pairs }|rest], config }) do
+  defp insert(value, { [], config }), do: { value, config }
+  defp insert(key, { [{ :object, pairs }|rest], config }) do
     { [{ :object, key, pairs }] ++ rest, config }
   end
-  def insert(value, { [{ :object, key, pairs }|rest], config }) do
+  defp insert(value, { [{ :object, key, pairs }|rest], config }) do
     { [{ :object, Map.put(pairs, key, value) }] ++ rest, config }
   end
-  def insert(value, { [{ :array, values}|rest], config }) do
+  defp insert(value, { [{ :array, values}|rest], config }) do
     { [{ :array, [value] ++ values}] ++ rest, config }
   end
-  def insert(_, _), do: raise ArgumentError
+  defp insert(_, _), do: raise ArgumentError
 
-  def get_value({ value, _config }), do: value
-  def get_value(_), do: raise ArgumentError
+  defp get_value({ value, _config }), do: value
+  defp get_value(_), do: raise ArgumentError
 end
 
 defprotocol JSEX.Encoder do
@@ -154,7 +154,7 @@ end
 defimpl JSEX.Encoder, for: List do
   def json([]), do: [:start_array, :end_array]
   def json([{}]), do: [:start_object, :end_object]
-  def json([{ key, _ }|_] = list) do
+  def json([{ _, _ }|_] = list) do
     [:start_object] ++
       flatten(for term <- unzip(list), do: JSEX.Encoder.json(term)) ++
     [:end_object]
@@ -163,10 +163,10 @@ defimpl JSEX.Encoder, for: List do
     [:start_array] ++ flatten(for term <- list, do: JSEX.Encoder.json(term)) ++ [:end_array]
   end
   
-  def unzip(list), do: unzip(list, [])
+  defp unzip(list), do: unzip(list, [])
     
-  def unzip([], acc), do: Enum.reverse(acc)
-  def unzip([{ key, value }|rest], acc)
+  defp unzip([], acc), do: Enum.reverse(acc)
+  defp unzip([{ key, value }|rest], acc)
   when is_binary(key) or is_atom(key) or is_integer(key) do
     unzip(rest, [value, key] ++ acc)
   end
