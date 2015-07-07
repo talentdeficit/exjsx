de8b0d771dfe1828c9d0b482951b55d3551e9a48
alisdair sullivan <alisdairsullivan@yahoo.ca>
2015-07-07 13:46:37 -0700

    remove obsolete `package.exs`

--- package.exs
+++
@@ -1,6 +0,0 @@
-Expm.Package.new(name: "exjsx", description: "json for elixir",
-                 version: "3.0.0", keywords: [json], 
-                 maintainers: [[name: "alisdair sullivan", 
-                                email: "alisdair sullivan@yahoo.ca"]],
-                 repositories: [[github: "talentdeficit/exjsx"]])
-    
