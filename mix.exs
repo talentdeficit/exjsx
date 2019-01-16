d98927e14695766dc7051d2041513bc5ed0297dc
okoriko <eurico@phybbit.com>
2019-01-16 09:30:17 +0900

    Relax the dependency requirement on jsx

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule EXJSX.Mixfile do
   end
 
   defp deps do
-    [{:jsx, "~> 2.8.0"}, {:ex_doc, "~> 0.14", only: :dev}]
+    [{:jsx, "~> 2.8"}, {:ex_doc, "~> 0.14", only: :dev}]
   end
 
   defp description do
